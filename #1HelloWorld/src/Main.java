9a38abe029dbc04299d78069c8f363e6f5e44741
Usman Sajid <67691352+code190715@users.noreply.github.com>
2021-01-10 22:13:48 +0000

    Add files via upload

---
+++ #1HelloWorld/src/Main.java
@@ -0,0 +1,33 @@
+/*Objective:
+ * Display to me the following exactly without using println:
+ 
+ * "	Hello Code190715"
+ * Lets begin!
+ 
+ */
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.print("\t\"Hello Code190715\"\n");
+		System.out.print("Lets begin!");
+	}
+}
+
+
+
+
+/*More Lesson Notes:
+ * 
+ * use sysout then tab for auto type System.out.print()
+ * 
+ * use Find & Replace when needed
+ * 
+ * use the \ symbol for escape sequence
+ * 
+ */
+	
+
+
+
