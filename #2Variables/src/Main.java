1ceef079ef2821a52f46afc897353554e55237a4
Usman Sajid <67691352+code190715@users.noreply.github.com>
2021-01-10 22:14:30 +0000

    Add files via upload

---
+++ #2Variables/src/Main.java
@@ -0,0 +1,79 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int x; 		// declaration
+		x = 123; 	// assignment
+		
+		int y = 123; //initialisation 
+		
+		System.out.println(x + y);
+		System.out.println("My number is " + x);
+		
+		long z = 123325464574257247L;
+		System.out.println("My number is " + z);
+		
+		double a = 123.01;
+		System.out.println("My number is " + a);
+		
+		float b = 3.14f;
+		System.out.println("My number is " + b);
+		
+		boolean c = true;
+		System.out.println("I am cool. " + c);
+		
+		char symbol = '@';
+		System.out.println(symbol);
+		
+		
+		String name = "Usman";
+		System.out.println("My name is " + name);
+		
+
+	}
+
+}
+
+
+/* Lesson Notes:
+ * 
+ * DATA TYPES:
+ * 
+ * >>>>Primitive
+ * >>> Object Reference
+ * 
+ * Primitive=
+ * store data
+ * can hold only one value
+ * faster
+ * 
+ * 
+ * 		boolean > true or false
+ * 		char
+ * 		bytes
+ * 		short
+ * 		int > -2billion to 2 billion
+ * 		long
+ * 		float
+ * 		double> decimals numbers
+ * 
+ * Reference=
+ * user defined, unlimited
+ * store address
+ * can hold multiple values
+ * slower
+ * 
+ * 
+ * HOW TO CREATE A VARIABLE:
+ * 
+ * Declaration		Declare data type & Give name		int x;
+ * Assignment		Given it value						x = 123;
+ * Initialisation	Both steps at once					int x = 123;
+ * Follow with semicolon
+ 
+ 
+ 
+ 
+ */
+ 
