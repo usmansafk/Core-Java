89f1ac44e06b2b8571a8a9f411bc44b4ee0c5f85
Usman Sajid <67691352+code190715@users.noreply.github.com>
2021-01-14 22:11:37 +0000

    Add files via upload

---
+++ #8RandomNumbers/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Random random = new Random();
+		
+		//int x = random.nextInt(6)+1;
+		//double y = random.nextDouble();
+		boolean z = random.nextBoolean();
+				
+		System.out.println(z);
+		
+
+	}
+
+}
+
+
+
+
+/* 
+
+Further Notes:
+
+Random class is-  import java.util.Random;
+
+*Random class generates only these are pseudo random numbers
+
+
+*/
