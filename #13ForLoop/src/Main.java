4f1c6fad3c10936668eea8eb5bed6f12cd02a6d8
Usman Sajid <67691352+code190715@users.noreply.github.com>
2021-01-17 20:23:56 +0000

    Add files via upload

---
+++ #13ForLoop/src/Main.java
@@ -0,0 +1,33 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+		for (int i=10; i>=0; i--) {
+			System.out.println(i);
+		}
+		
+		System.out.println("Eid Mubarak!");
+
+		
+		
+	}
+
+}
+
+
+
+/* 
+
+Further Notes:
+
+-for loop = executes a block of code a limited amount of time
+
+compared to a while loop,which  executes infinite amount of times, if condition is true,
+a for loop is used when we know how many times it will iterate over the block of code
+
+
+
+*/
+
