7fe8919606dd5abb2d50adbbb3d42d295cd7f21e
Usman Sajid <67691352+code190715@users.noreply.github.com>
2021-01-17 20:24:55 +0000

    Add files via upload

---
+++ #17StringMethods/src/Main.java
@@ -0,0 +1,60 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		String firstName = "Usman";
+		
+		
+		//useful Methods of String Class:
+		
+		//boolean result = name.equals("Usman");
+		//boolean result = name.equalsIgnoreCase("usman");
+		
+		//int result = name.length();
+		
+		//char result = name.charAt(3);
+		
+		//int result = name.indexOf("U");
+		
+		//boolean result = name.isEmpty();
+		
+		//String result = name.toUpperCase();
+		//String result = name.toLowerCase();
+		
+		String result = firstName.trim();
+		
+		//String result = name.replaceAll("Us", "Uth");
+		
+		System.out.println(result);
+		
+
+	}
+
+}
+
+
+
+/*  
+
+Further Notes:
+
+String class = 
+reference  data type, 
+used to store one or more characters
+reference data types have access to useful methods
+
+These are the useful mehtods covered:
+
+.equals > boolean
+.equalsIgnoreCase > boolean
+.length > int
+.charAt > char
+.indexOf > int
+.isEmpty > boolean
+.toUpperCase > String
+.toLowerCase > String
+.trim > String
+
+*/
