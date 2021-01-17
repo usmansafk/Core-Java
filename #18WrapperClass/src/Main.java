6a3cc1bdb01353aaef9c555d64a506bb8993716a
Usman Sajid <67691352+code190715@users.noreply.github.com>
2021-01-17 20:25:04 +0000

    Add files via upload

---
+++ #18WrapperClass/src/Main.java
@@ -0,0 +1,47 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		//these are wrapper classes
+		Boolean a = true;
+		Character b = '@';
+		Integer c = 3;
+		Double d = 3.145;
+		
+		String e = "Usman";
+	}
+
+}
+
+
+
+/* 
+
+Further Notes:
+
+wrapper classes = 
+provide a way to use primitive data  types as reference data types
++reference data types contain useful methods
++can be used with certain collections, like ArrayList
+-slower to accesses 
+
+Naming convention = First Letter is Capital and spelt in Full
+
+auto-boxing = automatic conversion the java compiler makes between primitive data types and their respected wrapper class
+
+un-boxing = reverse of auto-boxing. Automatic conversion of wrapper class to primitive
+
+
+Usefull methods:
+
+Boolean Methods:
+
+
+
+
+
+
+
+
+*/
