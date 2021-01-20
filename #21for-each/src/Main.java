dbfcb07adf4a2d733e614e9d32142895e5397bb7
Usman Sajid <67691352+code190715@users.noreply.github.com>
2021-01-20 20:19:48 +0000

    Add files via upload

---
+++ #21for-each/src/Main.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+
+		ArrayList<String> hope = new ArrayList<String>();
+		
+		hope.add("hope is cool");
+		hope.add("hope is nice");
+		hope.add("hope is friendly");
+		hope.add("hope is funny");
+		hope.add("hope is yummy");
+		hope.add("hope is lovely");
+		
+		for(String i : hope) {
+			System.out.println(i);
+		}
+	}
+
+}
+
+
+
+
+/* 
+
+Further Notes:
+
+foreach loop=
+
+ 
+ traversing technique to iterate through the elements in an array/collection
++less steps, more readable
+- less flexible 
+
+
+		
+		}
+		
+
+*/
