d856e7cb94c78c80fdaefae428406a5e694e3fbe
Usman Sajid <67691352+code190715@users.noreply.github.com>
2021-01-24 13:25:29 +0000

    Add files via upload

---
+++ #25final/src/Main.java
@@ -0,0 +1,30 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		final double pi = 3.14;
+		
+		System.out.println(pi);
+
+
+	}
+	
+}
+
+
+
+
+
+
+
+/* 
+
+Further Notes:
+
+final variables = cant be modified later in code.
+Common practice is variables declared final is to name them in all CAPS.
+		 
+		 
+*/
+
